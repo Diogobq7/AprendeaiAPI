7a3754f8b024d52d1fe64c6e4884cb102549b947
heloisa-machado <heloisa.machado@germinare.org.br>
2026-03-08 18:37:33 -0300

    fix: getProfessor

--- src/main/java/org/example/aprendeaiapi/repository/UsuarioRepository.java
+++ src/main/java/org/example/aprendeaiapi/repository/UsuarioRepository.java
@@ -37,12 +37,18 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     """, nativeQuery = true)
     List<AlunoResposeDTO> findAllAluno();
 
-    @Query("""
-        SELECT u.matricula, u.nomeCompleto, d.nomeDisciplina, u.email, t.anoEscolar FROM Usuario u
-        JOIN UsuarioTurma ut ON u.id = ut.usuario.id
-        JOIN Turma t ON ut.turma.id = t.id
-        JOIN Disciplina d ON t.disciplina.id = d.id
-        WHERE u.tipoUsuario = 'PROFESSOR'
-    """)
+    @Query(value = """
+    SELECT 
+        u.matricula,
+        u.nome_completo,
+        d.nome_disciplina,
+        u.email,
+        t.ano_escolar
+    FROM usuario u
+    JOIN usuario_turma ut ON u.id = ut.id_usuario
+    JOIN turma t ON ut.id_turma = t.id
+    JOIN disciplinas d ON t.id_disciplina = d.id
+    WHERE u.tipo_usuario = 'PROFESSOR'
+""", nativeQuery = true)
     List<ProfessorResposeDTO> findAllProfessor();
 }
