acb9525a4e34a81cda8ffd631b15da9494b78c3b
Diogobq7 <diogo.queiroz@germinare.org.br>
2026-03-13 09:32:11 -0300

    consertando erro de query

--- src/main/java/org/example/aprendeaiapi/repository/UsuarioRepository.java
+++ src/main/java/org/example/aprendeaiapi/repository/UsuarioRepository.java
@@ -40,7 +40,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     List<AlunoResposeDTO> findAllAluno();
 
     @Query(value = """
-    SELECT u.matricula, u.nome_completo, d.nome_disciplina, u.email, u.status  t.ano_escolar FROM usuario u
+    SELECT u.matricula, u.nome_completo, d.nome_disciplina, u.email, u.status, t.ano_escolar FROM usuario u
     JOIN usuario_turma ut ON u.id = ut.id_usuario
     JOIN turma t ON ut.id_turma = t.id
     JOIN turma_disciplina td ON td.id_turma = t.id
